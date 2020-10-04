7f844ec9e91cbd0e1c35be92b84b12a8fcecaf4b
Ubuntu <ubuntu@ip-172-31-91-123.ec2.internal>
2020-10-04 16:55:20 +0000

    Added in sum.py

---
+++ sum.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Created by Marcus A. Mosley
+# Created on October 2020
+# This program finds the sum of all natural numbers preceding the
+#     number inputted by the user
+
+
+def main():
+    # This function finds the sum of all natural numbers preceding the
+    #     number inputted by the user
+
+    # Input
+    counter = 0
+    sum = 0
+    natural_string = input("Enter a natural number (To Find Sum 1 to N): ")
+    print("")
+
+    # Process & Output
+    try:
+        natural_integer = int(natural_string)
+    except Exception:
+        print("You have not inputted an integer, please input an integer"
+              " (natural number)!")
+    else:
+        if natural_integer <= 0:
+            print("You have not inputted a positive number, please input a"
+                  " positive number!")
+        else:
+            while counter <= natural_integer:
+                sum = sum + counter
+                counter = counter + 1
+            print("The Sum of all natural numbers 1 to {0} is {1}"
+                  .format(natural_integer, sum))
+
+
+if __name__ == "__main__":
+    main()
